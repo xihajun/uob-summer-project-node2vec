27ef6902a3e8452cf47254ca6c538c4ca2c1c8d7
xihajun <www.junfan.com@gmail.com>
2019-07-10 10:35:11 +0100

    Create node.py

---
+++ node.py
@@ -0,0 +1,30 @@
+from subprocess import Popen, PIPE
+import glob
+import os
+import subprocess
+
+f_list = glob.glob('./node2vec_input/*.txt')
+
+# get the outputlist
+def outputlist(input_filename):
+    temp = input_filename.split('/')
+    temp[2] = 'node2vec_output'
+    return "/".join(temp)
+    
+    
+output_filename = [outputlist(f_list[i]) for i in range(len(f_list))]
+
+file_name = [[f_list[i],output_filename[i]] for i in range(len(f_list))]
+
+
+node2vec_input_dir = '.' + "/node2vec_output/"
+os.makedirs(node2vec_input_dir, exist_ok=True)
+
+for i in range(len(cmds_list)):
+    try:
+        subprocess.run(cmds_list[i])
+        temp = cmds_list[i].split(' ')
+        subprocess.run(["rm",temp[3]])
+    except:
+        print("fail")
+        
